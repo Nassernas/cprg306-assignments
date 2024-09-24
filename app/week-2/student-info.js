2fa389f9f7a44d41348b069813e54eeccc837446
Nassernas <nasser.nasser@edu.sait.ca>
2024-09-24 14:39:59 -0600

    week 2 assignment commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+
+  return (
+    <div>
+      <h3>Nasser Nasser</h3>
+      <Link href="https://github.com/Nassernas">My Github</Link>
+    </div>
+  );
+  
+}
