ecd85e9b43eb4612e0d40b252b13948a4c315d09
Nassernas <nasser.nasser@edu.sait.ca>
2024-10-22 21:04:55 -0600

    week 6 assignment commit

--- app/page.js
+++ app/page.js
@@ -20,6 +20,9 @@ export default function Home() {
             <li>
               <Link href="./week-5/" className={linkStyles}>Week 5 Assignment</Link>
             </li>
+            <li>
+              <Link href="./week-6/" className={linkStyles}>Week 6 Assignment</Link>
+            </li>
           </ul>
         </div>
       </main>
